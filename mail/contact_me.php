94d9ccac5865f11e05fd68d37543e201565b9bb5
Abhishek <abhisheksinghal7599@gmail.com>
2018-12-05 15:56:29 +0530

    Personalizing

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -4,19 +4,25 @@ if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || 
   http_response_code(500);
   exit();
 }
-
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-
 // Create the email and send the message
-$to = "yourname@yourdomain.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = "abhisheksinghal7599@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header = "From: abhisheksinghal421@yahoo.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
+function debug_to_console( $data ) {
+    $output = $data;
+    if ( is_array( $output ) )
+        $output = implode( ',', $output);
 
-if(!mail($to, $subject, $body, $header))
+    echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
+}
+if(!mail($to, $subject, $body, $header)){
   http_response_code(500);
+  debug_to_console( "ERRORORORORO" );
+}
 ?>
